9556c45cb7121295172516811f7baacdbc7514a9
OlsonNE-JBU <160153586+OlsonNE-JBU@users.noreply.github.com>
2024-02-15 10:01:00 -0600

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,38 @@
+function popupfunction(x)
+
+{
+
+document.getElementById('popup'+x).style.display='block';
+
+document.getElementById('fade'+x).style.display='block';
+
+}
+
+
+
+function popupclose(x)
+
+{
+
+document.getElementById('popup'+x).style.display='none';
+
+document.getElementById('fade'+x).style.display='none';
+
+}
+
+function showCart()
+{
+	document.getElementById('cartList').style.display='block';
+}
+
+function hideCart()
+{
+	document.getElementById('cartList').style.display='none';
+}
+
+function addtocart(x)
+
+{
+	document.getElementById
+	
+}
